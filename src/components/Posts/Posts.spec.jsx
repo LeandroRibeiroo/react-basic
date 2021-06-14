367da518ce373cbc409f4436a34c3132fce25970
Leandro Ribeiro <leandroribeiro.sa1997@gmail.com>
2021-06-14 16:42:40 -0300

    Add some tests

---
+++ src/components/Posts/Posts.spec.jsx
@@ -0,0 +1,43 @@
+const { render, screen } = require('@testing-library/react');
+import { Posts } from '.';
+
+const props = {
+  posts: [
+    {
+      id: 1,
+      title: 'Title 1',
+      body: 'Body 1',
+      cover: 'img/img1.png',
+    },
+    {
+      id: 2,
+      title: 'Title 2',
+      body: 'Body 2',
+      cover: 'img/img2.png',
+    },
+    {
+      id: 3,
+      title: 'Title 3',
+      body: 'Body 3',
+      cover: 'img/img3.png',
+    },
+  ],
+};
+describe('<Posts />', () => {
+  it('should render posts', () => {
+    render(<Posts {...props} />);
+    expect(screen.getAllByRole('heading', { name: /title/i })).toHaveLength(3);
+    expect(screen.getAllByRole('img', { name: /title/i })).toHaveLength(3);
+    expect(screen.getAllByText(/body/i)).toHaveLength(3);
+  });
+
+  it('should not render any posts', () => {
+    render(<Posts />);
+    expect(screen.queryAllByRole('heading', { name: /title/i })).toHaveLength(0);
+  });
+
+  it('should match snapshot', () => {
+    const { container } = render(<Posts {...props} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,64 @@
+import './styles.css';
+import { useCallback, useEffect, useState } from 'react';
+import { loadPosts } from '../../components/utilities/loadPosts';
+import { Posts } from '../../components/Posts';
+import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
+
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(5);
+  const [searchValue, setSearchValue] = useState('');
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = searchValue
+    ? allPosts.filter((post) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    : posts;
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+    const photosAndPosts = await loadPosts();
+
+    setPosts(photosAndPosts.slice(page, postsPerPage));
+    setAllPosts(photosAndPosts);
+  }, []);
+
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
+
+  const loadMorePosts = () => {
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    setPosts(posts);
+    setPage(nextPage);
+  };
+
+  return (
+    <section className="container">
+      <div className="search-container">
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
+      </div>
+
+      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+      {filteredPosts.length === 0 && <p>Nothing was found in our database.</p>}
+
+      <div className="button-container">
+        {!searchValue && <Button text="Load more posts" onClick={loadMorePosts} disabled={noMorePosts} />}
+      </div>
+    </section>
+  );
+};
+
+export default Home;
